893ce70d247e3ef15e7be6347ee522226730ba34
Tyler D <drelyt@gmail.com>
2022-02-04 12:34:34 -0500

    created basic structure

---
+++ pages/create.jsx
@@ -0,0 +1,11 @@
+export default function Create() {
+  return (
+    <div>
+      <h1>Create Your Adventure</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae a
+        quibusdam porro modi aliquid alias. Id iusto laborum ad.
+      </p>
+    </div>
+  );
+}
